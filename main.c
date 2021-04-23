216a20afce3003ff50f705f050d4dea9ae8909d7
Benjamin Prince <princebe@os1.engr.oregonstate.edu>
2021-04-22 17:37:04 -0700

    added global variables

--- main.c
+++ main.c
@@ -8,8 +8,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define True 1
+#define False 0
+#define MAX_CHARS 2048
+#define MAX_ARGS 512
+
+void userInput() {
+
+    printf(": ");
+    scanf()
+
+
+}
+
+
 int main(int argc, char *argv[]) {
 
-    printf("Hello World!\n");
+    printf("Hello World!\n");  // TODO: Remember to remove
+
+
+    while (True) {
+
+        userInput();
+
+    }
 
 }
