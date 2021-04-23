1524a4e41ce19946779531bfb2fd788af3b732a0
Benjamin Prince <princebe@os1.engr.oregonstate.edu>
2021-04-22 17:15:23 -0700

    first main commit

---
+++ main.c
@@ -0,0 +1,15 @@
+// Author: Ben Prince
+// Assignment 3: Smallsh
+// Date: 05/03/2021
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+    printf("Hello World!\n");
+
+}
