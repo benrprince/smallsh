ae06d58097b152aab9f593c0d2f7c4e41e61a95e
Benjamin Prince <princebe@os1.engr.oregonstate.edu>
2021-04-22 18:31:00 -0700

    userInput function

--- main.c
+++ main.c
@@ -8,29 +8,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define True 1
-#define False 0
 #define MAX_CHARS 2048
 #define MAX_ARGS 512
 
+int next = 1;
+
 void userInput() {
 
-    printf(": ");
-    scanf()
 
+    char input[MAX_CHARS];
+
+    // Get input from user
+    printf(": ");
+    fflush(stdout);
+    fgets(input, sizeof(input), stdin);
+
+    // Check if input is exit
+    if (strncmp(input, "exit", 4) == 0) {
+
+        next = 0;
+        return;
+
+    } else {
+
+    printf("%s", input);
+
+    }
+
+    return;
 
 }
 
 
 int main(int argc, char *argv[]) {
 
-    printf("Hello World!\n");  // TODO: Remember to remove
+    char input[MAX_CHARS];
 
-
-    while (True) {
+    while (next == 1) {
 
         userInput();
 
     }
+
+    return EXIT_SUCCESS;
 
 }
